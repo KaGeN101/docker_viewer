debcccda5dcb34f4b2d4fe39412a799d646a6807
Johann Eloff <kagen101@gmail.com>
2019-07-03 23:04:24 +0200

    Updated call to use docker registry api

--- docker_view/app/controllers/history_controller.rb
+++ docker_view/app/controllers/history_controller.rb
@@ -1,11 +1,18 @@
+require 'rest_client'
+
 class HistoryController < ApplicationController
   def index
     @name = params[:name]
-    output = []
-    IO.popen("docker history #{params[:name]}").each do |line|
-      output << line.chomp
-    end
+    @tag = params[:tag]
+    puts "#{ENV["REGISTRY"]}/v2/#{@name}/manifest/#{@tag}"
+    response =  RestClient::Request.execute method: :get, url: "#{ENV["REGISTRY"]}/v2/#{@name}/manifests/#{@tag}", user: ENV['USERNAME'], password: ENV['PASSWORD']
+    hash = JSON.parse response.to_str
     @history = ''
-    output.each {|line| @history += line + "\n"}
+    hash['history'].each do |history_hash| 
+      v1_comp = JSON.parse history_hash['v1Compatibility']
+      #p v1_comp['container_config']['Cmd'][0]
+      line = v1_comp['id'].strip + "\t" + v1_comp['container_config']['Cmd'][0]
+      @history += line + "\n"
+    end  
   end
 end
--- scripts/start_me.sh
+++ scripts/start_me.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
 
-cd ../docker_view
-rails s
+export REGISTRY="http://localhost:5000"
+export USERNAME="dev_user"
+export PASSWORD="123456"
+
+source ~/.rvm/scripts/rvm
+
+rvm use 2.5.5
+
+cd ../docker_view/ && bin/rails s
